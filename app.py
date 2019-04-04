f976b999a9f97c74b56b44ec3c7f421994a111aa
junyoo96 <junyoo96@gmail.com>
2019-04-04 02:54:58 +0000

    add app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask import jsonify
+
+#Oracle
+import cx_Oracle
 
 app=Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome!'
+	return 'Welcome'
+
+@app.route('/json')
+def json_test():
+	data={'hello' : 'world', 'number':3}
+	resp=jsonify(data)	
+	return resp
+
+@app.route('/dbtest')
+def db_test():
+	con=cx_Oracle.connect('scott/12345678@oracle11gr2.cdubaygxhxxb.ap-northeast-2.rds.amazonaws.com:1521/orcl')
+	print (con.version)
+	con.close()	
 
 if __name__=='__main__':
-    app.run()
+	app.run(host='0.0.0.0',port='5000')
